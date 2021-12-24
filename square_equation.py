b2377621da011a224c66574c863da0a750b18b3a
Alexei Storozhev <65788722+strox812@users.noreply.github.com>
2021-12-24 08:27:53 +0800

    Update square_equation.py

--- square_equation.py
+++ square_equation.py
@@ -1,4 +1,4 @@
-from math import pow
+from math import pow, sqrt
 a, b, c = float(input()), float(input()), float(input())
 d = pow(b, 2) - (4 * a * c)
 
@@ -8,6 +8,6 @@ elif d == 0:
     x = - (b) / (2 * a)
     print(x)
 elif d > 0:
-    x_1 = (-(b) + pow(d, 2)) / (2 * a)
-    x_2 = (-(b) - pow(d, 2)) / (2 * a)
+    x_1 = (- b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a)
+    x_2 = (- b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a)
     print(min(x_1, x_2), max(x_1, x_2), sep='\n')
